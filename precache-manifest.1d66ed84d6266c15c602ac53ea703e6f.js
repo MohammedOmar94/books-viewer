010c63ee9c3683694b14ddbef827ef5ffbf5882c
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-04-20 23:27:35 +0100

    Updates

---
+++ precache-manifest.1d66ed84d6266c15c602ac53ea703e6f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/books-viewer/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/books-viewer/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/books-viewer/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/books-viewer/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/books-viewer/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "666a6ad0c6b3b2aa9e88",
+    "url": "/books-viewer/static/js/runtime~main.50813e9d.js"
+  },
+  {
+    "revision": "bd7334d4fcd34da93613",
+    "url": "/books-viewer/static/js/main.3c15b7ea.chunk.js"
+  },
+  {
+    "revision": "4cc0400bda30522f3094",
+    "url": "/books-viewer/static/js/2.a7ee951f.chunk.js"
+  },
+  {
+    "revision": "bd7334d4fcd34da93613",
+    "url": "/books-viewer/static/css/main.08d9c4af.chunk.css"
+  },
+  {
+    "revision": "f417066197cc997eb2b7429dbd3b87c5",
+    "url": "/books-viewer/index.html"
+  }
+];
