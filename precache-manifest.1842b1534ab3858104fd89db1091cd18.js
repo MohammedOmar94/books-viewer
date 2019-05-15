86ae36d015f00e32ea81a171dc068071181eba1d
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-05-15 16:26:37 +0100

    Updates

--- precache-manifest.1842b1534ab3858104fd89db1091cd18.js
+++ precache-manifest.1842b1534ab3858104fd89db1091cd18.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "/books-viewer/static/media/glyphicons-halflings-regular.fa277232.woff"
   },
   {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/books-viewer/static/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
     "revision": "f4769f9bdb7466be65088239c12046d1",
     "url": "/books-viewer/static/media/glyphicons-halflings-regular.f4769f9b.eot"
   },
   {
     "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
     "url": "/books-viewer/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
-  },
-  {
-    "revision": "89889688147bd7575d6327160d64e760",
-    "url": "/books-viewer/static/media/glyphicons-halflings-regular.89889688.svg"
   },
   {
     "revision": "448c34a56d699c29117adc64c43affeb",
@@ -24,19 +24,19 @@ self.__precacheManifest = [
     "url": "/books-viewer/static/js/runtime~main.50813e9d.js"
   },
   {
-    "revision": "a59b00fea89c228e7079",
-    "url": "/books-viewer/static/js/main.58e55fad.chunk.js"
+    "revision": "f95bda3ccf528357b72f",
+    "url": "/books-viewer/static/js/main.4764350f.chunk.js"
   },
   {
     "revision": "4cc0400bda30522f3094",
     "url": "/books-viewer/static/js/2.a7ee951f.chunk.js"
   },
   {
-    "revision": "a59b00fea89c228e7079",
-    "url": "/books-viewer/static/css/main.08d9c4af.chunk.css"
+    "revision": "f95bda3ccf528357b72f",
+    "url": "/books-viewer/static/css/main.9a27638a.chunk.css"
   },
   {
-    "revision": "bab179aad0a3441333b507212165c923",
+    "revision": "18fdd100f3af20abed5a297f83fc7153",
     "url": "/books-viewer/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/books-viewer/precache-manifest.06ca09012906db30b581fcc6d3b08f96.js"
+  "/books-viewer/precache-manifest.1842b1534ab3858104fd89db1091cd18.js"
 );
 
 workbox.clientsClaim();
