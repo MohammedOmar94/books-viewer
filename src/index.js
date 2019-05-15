80f832d503f69331f8b4b8240dce07f4a2a97bb7
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-05-15 16:14:05 +0100

    API calls should now work on GH pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // All axios API requests will have this prefixed automatically.
-axios.defaults.baseURL = '//nyx.vima.ekt.gr:3000/';
+axios.defaults.baseURL = 'https://cors-anywhere.herokuapp.com/http://nyx.vima.ekt.gr:3000/';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
